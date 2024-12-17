6e3e557514aecf4304bdd4d7d5f6f2f0d64aa43c
Emre YURTTAS <emre.yurttas@etudiant.univ-reims.fr>
2024-12-17 09:40:35 +0100

    activation du débogage

--- src/config.js
+++ src/config.js
@@ -1,10 +1,16 @@
+
 import Phaser from "phaser";
 import Title from "./scene/Title.js";
 import Level1 from "./scene/Level1.js";
 
 export default {
     type: Phaser.AUTO,
-    physics: { default: "arcade" },
+    physics: {
+        default: "arcade",
+        arcade: {
+            debug: true
+        },
+    },
     scale: {
         mode: Phaser.Scale.FIT,
         width: 800,
