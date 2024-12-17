38941a5796d19cb8b95fc6a85bfd810dd96d4c16
Emre YURTTAS <emre.yurttas@etudiant.univ-reims.fr>
2024-12-17 10:56:39 +0100

    ajout du premier niveau avec la lave (4.)

--- src/scene/Level1.js
+++ src/scene/Level1.js
@@ -1,22 +1,38 @@
 import Phaser from "phaser";
 import StoneGroup from "../object/StoneGroup.js";
+import LavaGroup from "../object/LavaGroup.js"; // Importer LavaGroup
 
 export default class Level1 extends Phaser.Scene {
     constructor() {
         super({ key: "Level1" });
     }
+
     preload() {
         StoneGroup.preload(this);
+        LavaGroup.preload(this);
     }
 
     create() {
         this.add.text(400, 50, "Niveau 1").setOrigin(0.5);
 
         this.stoneGroup = new StoneGroup(this);
+        this.stoneGroup.addTiles(0, 8, 4);
+        this.stoneGroup.addTiles(8, 8, 5);
+        this.stoneGroup.addTiles(2, 7, 1);
+        this.stoneGroup.addTiles(4, 6, 1);
+        this.stoneGroup.addTiles(6, 5, 3);
+        this.stoneGroup.addTiles(3, 1, 3);
+        this.stoneGroup.addTiles(7, 3);
+        this.stoneGroup.addTiles(9, 7, 1);
+        this.stoneGroup.addTiles(11, 6, 1);
 
-        this.stoneGroup.addTiles(2,3,3);
+        this.lavaGroup = new LavaGroup(this);
+        this.lavaGroup.addTiles(4, 8, 4)
 
         this.stoneGroup.children.iterate((tile) => {
+            tile.refreshBody();
+        });
+        this.lavaGroup.children.iterate((tile) => {
             tile.refreshBody();
         });
     }
