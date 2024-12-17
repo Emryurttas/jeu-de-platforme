fa866922f3415f0d10957c610118625a945eab64
Emre YURTTAS <emre.yurttas@etudiant.univ-reims.fr>
2024-12-17 09:30:49 +0100

    ajout de la méthode preload

--- src/object/StoneGroup.js
+++ src/object/StoneGroup.js
@@ -4,4 +4,7 @@ export default class StoneGroup extends TileGroup{
     constructor(scene) {
         super(scene, 'stone', 64, 64);
     }
+    static preload(scene) {
+        scene.load.image('stone', 'imj/stone.png');
+    }
 }
