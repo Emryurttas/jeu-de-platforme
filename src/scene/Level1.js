ff1fbdc8c2a2d9d65e4cc171f9845f6ee15d76c9
Emre YURTTAS <emre.yurttas@etudiant.univ-reims.fr>
2024-12-17 10:32:11 +0100

    ajout des 3 blocs

--- src/scene/Level1.js
+++ src/scene/Level1.js
@@ -1,12 +1,23 @@
 import Phaser from "phaser";
-
+import StoneGroup from "../object/StoneGroup.js";
 
 export default class Level1 extends Phaser.Scene {
     constructor() {
-        super({ key: 'Level1' });
+        super({ key: "Level1" });
+    }
+    preload() {
+        StoneGroup.preload(this);
     }
 
     create() {
-        this.add.text(400, 300, 'Niveau 1').setOrigin(0.5);
+        this.add.text(400, 50, "Niveau 1").setOrigin(0.5);
+
+        this.stoneGroup = new StoneGroup(this);
+
+        this.stoneGroup.addTiles(2,3,3);
+
+        this.stoneGroup.children.iterate((tile) => {
+            tile.refreshBody();
+        });
     }
 }
