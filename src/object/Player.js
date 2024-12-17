ca40c5ee3d385a185091d6e02affc6bfeaadb0c2
Emre YURTTAS <emre.yurttas@etudiant.univ-reims.fr>
2024-12-17 12:03:31 +0100

    possibilité de supprimer le précedent avant d'en créer un nouveau tween

--- src/object/Player.js
+++ src/object/Player.js
@@ -13,12 +13,16 @@ export default class Player extends Phaser.Physics.Arcade.Sprite {
 
         this.setSize(40, 40);
         this.setCollideWorldBounds(true);
+        this.tween = null;
 
     }
     static preload(scene) {
         scene.load.spritesheet("player", "/img/player.png", { frameWidth: 40, frameHeight: 40 });
     }
     #move(velocity) {
+        if (this.moveTween) {
+            this.moveTween.destroy();
+        }
         this.moveTween = this.scene.tweens.add({
             targets: this.body.velocity,
             x: velocity,
@@ -32,13 +36,14 @@ export default class Player extends Phaser.Physics.Arcade.Sprite {
         this.#move(-300);
     }
     halt(){
+        if (this.moveTween) {
+            this.moveTween.destroy();
+        }
         this.moveTween = this.scene.tweens.add({
             targets: this.body.velocity,
             x: 0,
             duration: 300,
         });
     }
-
-
 
 }
