e5a9b736df23d683d174dba227ce884603125844
Emre YURTTAS <emre.yurttas@etudiant.univ-reims.fr>
2024-12-17 09:30:37 +0100

    ajout de la méthode addTiles

--- src/object/TileGroup.js
+++ src/object/TileGroup.js
@@ -1,15 +1,23 @@
 import Phaser from "phaser";
 
 export default class TileGroup extends Phaser.Physics.Arcade.StaticGroup {
-    constructor(scene, tileName, tileWidth,tileHeight) {
-        super(scene, tileName, tileWidth,tileHeight);
+    constructor(scene, tileName, tileWidth, tileHeight) {
+        super(scene);
         this.tileName = tileName;
         this.tileWidth = tileWidth;
         this.tileHeight = tileHeight;
-
     }
 
     addTiles(x, y, width = 1, height = 1) {
-
+        for (let i = 0; i < width; i++) {
+            for (let j = 0; j < height; j++) {
+                this.create(
+                    x * this.tileWidth + (i * this.tileWidth),
+                    y * this.tileHeight + (j * this.tileHeight),
+                    this.tileName
+                );
+            }
+        }
+        return this;
     }
 }
