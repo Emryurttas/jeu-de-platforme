9eeb4993bccd84b4898e2af811ccec785d1a9c0e
Emre YURTTAS <emre.yurttas@etudiant.univ-reims.fr>
2024-12-17 10:30:23 +0100

    Ajout origine du bloc dans le coin supérieur à gauche

--- src/object/TileGroup.js
+++ src/object/TileGroup.js
@@ -2,7 +2,7 @@ import Phaser from "phaser";
 
 export default class TileGroup extends Phaser.Physics.Arcade.StaticGroup {
     constructor(scene, tileName, tileWidth, tileHeight) {
-        super(scene);
+        super(scene.physics.world, scene);
         this.tileName = tileName;
         this.tileWidth = tileWidth;
         this.tileHeight = tileHeight;
@@ -11,11 +11,12 @@ export default class TileGroup extends Phaser.Physics.Arcade.StaticGroup {
     addTiles(x, y, width = 1, height = 1) {
         for (let i = 0; i < width; i++) {
             for (let j = 0; j < height; j++) {
-                this.create(
+                const tile = this.create(
                     x * this.tileWidth + (i * this.tileWidth),
                     y * this.tileHeight + (j * this.tileHeight),
                     this.tileName
                 );
+                tile.setOrigin(0, 0);
             }
         }
         return this;
