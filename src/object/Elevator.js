a0c3a225e0d4deefc62228f9f3f053767024a847
Yurttas Emre <emre.yurttas@etudiant.univ-reims.fr>
2025-01-03 16:27:35 +0100

    Ajout de la méthode moveIn

--- src/object/Elevator.js
+++ src/object/Elevator.js
@@ -27,4 +27,27 @@ export default class Elevator extends Phaser.Physics.Arcade.StaticGroup {
             repeat: 0,
         });
     }
+    moveIn(player) {
+        this.scene.input.keyboard.enabled = false;
+        player.halt();
+
+        this.back.anims.play("open");
+
+        const elevatorCenterX = this.back.x + this.back.width / 5;
+        const elevatorCenterY = this.back.y + this.back.height / 3;
+
+        player.setDepth(this.back.depth + 1);
+        this.scene.tweens.add({
+            targets: player,
+            x: elevatorCenterX,
+            y: elevatorCenterY,
+            duration: 500,
+            ease: 'Linear',
+            onComplete: () => {
+                player.body.setAllowGravity(true);
+                this.scene.input.keyboard.enabled = true;
+                this.scene.scene.restart();
+            }
+        });
+    }
 }
