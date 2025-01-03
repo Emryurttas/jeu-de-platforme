846650a24ddca49f979aabff1fb41d66531ff9fe
Yurttas Emre <emre.yurttas@etudiant.univ-reims.fr>
2025-01-03 17:00:43 +0100

    Ajout de l'ascenseur à la scène

--- src/scene/Level1.js
+++ src/scene/Level1.js
@@ -1,4 +1,5 @@
 import Phaser from "phaser";
+import Elevator from "../object/Elevator.js";
 import StoneGroup from "../object/StoneGroup.js";
 import LavaGroup from "../object/LavaGroup.js";
 import Player from "../object/Player.js";
@@ -14,6 +15,7 @@ export default class Level1 extends Phaser.Scene {
         LavaGroup.preload(this);
         Player.preload(this);
         Background.preload(this);
+        Elevator.preload(this);
     }
 
     create() {
@@ -23,7 +25,7 @@ export default class Level1 extends Phaser.Scene {
 
         this.stoneGroup = new StoneGroup(this);
         this.stoneGroup.addTiles(0, 8, 4);
-        this.stoneGroup.addTiles(8, 8, 5);
+        this.stoneGroup.addTiles(8, 8, 14);
         this.stoneGroup.addTiles(2, 7, 1);
         this.stoneGroup.addTiles(4, 6, 1);
         this.stoneGroup.addTiles(6, 5, 3);
@@ -41,11 +43,18 @@ export default class Level1 extends Phaser.Scene {
         this.lavaGroup.children.iterate((tile) => {
             tile.refreshBody();
         });
+
         this.player = new Player(this, 40, 280);
         this.physics.add.collider(this.player, this.stoneGroup);
-
         this.physics.add.overlap(this.player, this.lavaGroup, this.handleLavaCollision, null, this);
 
+        this.elevator = new Elevator(this);
+        this.elevator.back.setPosition(21 * 64, 7 * 64);
+
+        this.elevator.back.body.setSize(this.elevator.back.width, this.elevator.back.height);
+
+
+        this.physics.add.existing(this.elevator.back, true);
 
         this.cameras.main.startFollow(this.player, true, 0.1, 0.1);
         this.setBounds(0, 0, 1472, 640);
@@ -53,10 +62,13 @@ export default class Level1 extends Phaser.Scene {
         this.keys = this.input.keyboard.addKeys({
             left: Phaser.Input.Keyboard.KeyCodes.LEFT,
             right: Phaser.Input.Keyboard.KeyCodes.RIGHT,
-            space: Phaser.Input.Keyboard.KeyCodes.SPACE
+            space: Phaser.Input.Keyboard.KeyCodes.SPACE,
+            down: Phaser.Input.Keyboard.KeyCodes.DOWN,
         });
+
         this.handleInput();
     }
+
     handleInput() {
         this.keys.left.on("down", () => this.handleMove());
         this.keys.left.on("up", () => this.handleMove());
@@ -65,7 +77,7 @@ export default class Level1 extends Phaser.Scene {
         this.keys.right.on("up", () => this.handleMove());
 
         this.keys.space.on("down", () => this.handleJump());
-
+        this.keys.down.on("down", () => this.#handleInteract());
     }
 
     handleMove() {
@@ -94,5 +106,13 @@ export default class Level1 extends Phaser.Scene {
     setBounds(x, y, width, height) {
         this.cameras.main.setBounds(x, y, width, height);
         this.physics.world.setBounds(x, y, width, height);
+    }
+
+    #handleInteract() {
+        const distance = Phaser.Math.Distance.Between(this.player.x, this.player.y, this.elevator.back.x, this.elevator.back.y);
+
+        if (distance < 50 && this.player.body.onFloor()) {
+            this.elevator.moveIn(this.player);
+        }
     }
 }
