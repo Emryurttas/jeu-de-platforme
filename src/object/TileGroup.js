fd991204180d88a05d05c41dd1f7739790aeac32
Yurttas Emre <emre.yurttas@etudiant.univ-reims.fr>
2025-01-05 00:31:59 +0100

    adaptation pour les échelles

--- src/object/TileGroup.js
+++ src/object/TileGroup.js
@@ -1,19 +1,21 @@
 import Phaser from "phaser";
 
 export default class TileGroup extends Phaser.Physics.Arcade.StaticGroup {
-    constructor(scene, tileName, tileWidth, tileHeight) {
+    constructor(scene, tileName, tileWidth, tileHeight, offsetX = 0, offsetY = 0) {
         super(scene.physics.world, scene);
         this.tileName = tileName;
         this.tileWidth = tileWidth;
         this.tileHeight = tileHeight;
+        this.offsetX = offsetX;
+        this.offsetY = offsetY;
     }
 
     addTiles(x, y, width = 1, height = 1) {
         for (let i = 0; i < width; i++) {
             for (let j = 0; j < height; j++) {
                 const tile = this.create(
-                    x * this.tileWidth + (i * this.tileWidth),
-                    y * this.tileHeight + (j * this.tileHeight),
+                    (x + i) * this.tileWidth + this.offsetX,
+                    (y + j) * this.tileHeight + this.offsetY,
                     this.tileName
                 );
                 tile.setOrigin(0, 0);
